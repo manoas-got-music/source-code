d1db6942357da8f0994aa9eceb62ba2193d7c448
Alexander Edwards <alex44@hawaii.edu>
2025-04-24 20:32:21 -1000

    es lint

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
-import UserProfile from '../components/UserProfile';
+import UserProfile from '../../components/UserProfile';
 
 export default function ProfilePage() {
   const { data: session } = useSession();
